3ef2ca0cf3f5166006ac1fb544af5f256bfb6df2
Huỳnh Duy Quốc <nhoxkenl090@gmail.com>
2021-05-26 18:17:48 +0700

    update env

--- src/routes/web.js
+++ src/routes/web.js
@@ -4,6 +4,9 @@ const chatBotController = require("../controllers/chatBotController");
 const homePageController = require('../controllers/homePageController');
 let router = express.Router();
 
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
+
 let initWebRoutes = (app) => {
     // router.get("/", (req, res) => {
     //     return res.json("Hello word");//json - sendFile - send 

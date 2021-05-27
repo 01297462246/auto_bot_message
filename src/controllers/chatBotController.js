52dc9500574b950a98830a47a719aa49fe01d73a
Huỳnh Duy Quốc <nhoxkenl090@gmail.com>
2021-05-27 10:23:22 +0700

    update env

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -5,72 +5,51 @@ const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 // Handles messages events
-// function handleMessage(sender_psid, received_message) {
-//     let response;
-
-//     // Checks if the message contains text
-//     if (recvvceived_message.text) {
-//         console.log("----------------------------");
-//         console.log("DATA: " + received_message);
-//         console.log("----------------------------");
-// b
-//         // Create the payload for a basic text message, which
-//         // will be added to the body of our request to the Send API
-//         response = {
-//             "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
-//         }
-//     } else if (received_message.attachments) {
-//         // Get the URL of the message attachment
-//         let attachment_url = received_message.attachments[0].payload.url;
-//         response = {
-//             "attachment": {
-//                 "type": "template",
-//                 "payload": {
-//                     "template_type": "generic",
-//                     "elements": [{
-//                         "title": "Is this the right picture?",
-//                         "subtitle": "Tap a button to answer.",
-//                         "image_url": attachment_url,
-//                         "buttons": [
-//                             {
-//                                 "type": "postback",
-//                                 "title": "Yes!",
-//                                 "payload": "yes",
-//                             },
-//                             {
-//                                 "type": "postback",
-//                                 "title": "No!",
-//                                 "payload": "no",
-//                             }
-//                         ],
-//                     }]
-//                 }
-//             }
-//         }
-//     }
-
-//     // Send the response message
-//     callSendAPI(sender_psid, response);
-// }
-
-// Handles messaging_postbacks events
-
 function handleMessage(sender_psid, received_message) {
-
     let response;
-    console.log("================================");
-    console.log("Received: " + response);
-    console.log("================================");
-    // Check if the message contains text
-    if (received_message.text) {
 
-        // Create the payload for a basic text message
+    // Checks if the message contains text
+    if (recvvceived_message.text) {
+        console.log("----------------------------");
+        console.log("DATA: " + received_message);
+        console.log("----------------------------");
+        b
+        // Create the payload for a basic text message, which
+        // will be added to the body of our request to the Send API
         response = {
-            "text": `You sent the message: "${received_message.text}". Now send me an image!`
+            "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+        }
+    } else if (received_message.attachments) {
+        // Get the URL of the message attachment
+        let attachment_url = received_message.attachments[0].payload.url;
+        response = {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": "Is this the right picture?",
+                        "subtitle": "Tap a button to answer.",
+                        "image_url": attachment_url,
+                        "buttons": [
+                            {
+                                "type": "postback",
+                                "title": "Yes!",
+                                "payload": "yes",
+                            },
+                            {
+                                "type": "postback",
+                                "title": "No!",
+                                "payload": "no",
+                            }
+                        ],
+                    }]
+                }
+            }
         }
     }
 
-    // Sends the response message
+    // Send the response message
     callSendAPI(sender_psid, response);
 }
 
